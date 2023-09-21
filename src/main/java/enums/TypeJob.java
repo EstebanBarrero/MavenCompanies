1c22e0aefe9107177fc03a0580b1b8b9be4ffcee
EstebanBarrero <barrerodavid10@hotmail.com>
2023-09-20 22:23:43 -0500

    commit 004, Enum

--- src/main/java/enums/TypeJob.java
+++ src/main/java/enums/TypeJob.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package enums;
 
-/**
- *
- * @author USUARIO
- */
-public class TypeJob {
-          DIRECTIVO("D"), ASISTENCIAL("A"), OPERATIVO("O");
+public enum TypeJob {
+    DIRECTIVO("D"), ASISTENCIAL("A"), OPERATIVO("O");
     private final String codigo;
 
     TypeJob(String codigo) {
